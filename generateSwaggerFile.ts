32dfb5ed7a1509af23a285160982c792c5fcb214
Paul Beauchamp <paulb896@gmail.com>
2024-12-30 14:20:51 -0800

    Update swagger docs

--- generateSwaggerFile.ts
+++ generateSwaggerFile.ts
@@ -1,6 +1,9 @@
 import { server } from "./server/server";
 import { writeFileSync } from "fs";
 
+/**
+ * Generate a swagger file based on the server swagger configs.
+ */
 server.ready(() => {
   const yaml = server.swagger({ yaml: true });
   writeFileSync('./swagger.yml', yaml);
